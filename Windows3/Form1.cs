594d653fd84301e8d240c0b77f33f4c2e0cdd70a
long trần <pelong16042004@gmail.com>
2024-11-26 15:57:03 +0700

    Add project files.

---
+++ Windows3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem listItem = new ListViewItem(textBox1.Text);
+            listItem.SubItems.Add(textBox2.Text);
+            listItem.SubItems.Add(textBox3.Text);
+            listView1.Items.Add(listItem);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                foreach (ListViewItem item in listView1.SelectedItems)
+                {
+                    listView1.Items.Remove(item);
+                }
+            }
+            else
+            {
+                MessageBox.Show("hay chon it nhat 1 hang de xoa");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                foreach (ListViewItem item in listView1.SelectedItems)
+                {
+                    item.Text = textBox1.Text;
+                    item.SubItems[1].Text = textBox2.Text;
+                    item.SubItems[2].Text = textBox3.Text;
+                }
+
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to edit.");
+            }
+
+        }
+    }
+}
